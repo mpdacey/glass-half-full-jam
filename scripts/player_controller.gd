5e14bdbcc48144c4987f5c0be720dd9ab71a146d
mpdacey <80190692+mpdacey@users.noreply.github.com>
2025-11-27 13:19:56 +1100

    The player vehicle follows the mouse at a fixed speed.
    Hopefully will add some acceleration to the this movement.

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -2,6 +2,11 @@ extends Node3D
 class_name PlayerVehicleController
 
 @export var max_swerve_distance : float = 15.0
+@export var mouse_tracking_speed : float = 10
+var remapped_vehicle_position := 0.0
+
+func _physics_process(delta: float) -> void:
+	position.z = move_toward(position.z, remapped_vehicle_position, mouse_tracking_speed * delta)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is not InputEventMouseMotion:
@@ -12,5 +17,4 @@ func _unhandled_input(event: InputEvent) -> void:
 	var window_size := DisplayServer.window_get_size()
 	var mouse_coords_ratio := mouse_coords.x / window_size.x
 	
-	var remapped_vehicle_position := remap(mouse_coords_ratio, 0, 1, -max_swerve_distance, max_swerve_distance)
-	position.z = remapped_vehicle_position
+	remapped_vehicle_position = remap(mouse_coords_ratio, 0, 1, -max_swerve_distance, max_swerve_distance)

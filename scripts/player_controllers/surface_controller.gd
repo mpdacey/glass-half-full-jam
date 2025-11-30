197aa472b3dbbbfaa436684a239c691ed1475a92
mpdacey <80190692+mpdacey@users.noreply.github.com>
2025-11-30 12:51:16 +1100

    Created surface controller
    The surface controller reads the surface type from below the car and emits the change in surface type.

---
+++ scripts/player_controllers/surface_controller.gd
@@ -0,0 +1,35 @@
+extends Node
+class_name SurfaceController
+
+const ROAD_COLLISION_LAYER = 5
+
+signal road_type_changed(road_type: SurfaceType)
+
+enum SurfaceType {
+	ROAD,
+	DIRT,
+	OIL
+}
+
+@export var road_raycast: RayCast3D 
+var _null_surface_type := SurfaceType.DIRT
+var _current_surface_type := SurfaceType.ROAD:
+	set(value):
+		if _current_surface_type != value:
+			print("Surface type changed to : ", str(SurfaceType.keys()[value]).capitalize())
+			road_type_changed.emit(value)
+		_current_surface_type = value
+
+func _physics_process(_delta: float) -> void:
+	if not road_raycast.is_colliding():
+		_current_surface_type = _null_surface_type
+		return
+	
+	var road_collider_object := road_raycast.get_collider()
+	if road_collider_object is not CollisionObject3D:
+		return
+	
+	var road_collider := road_collider_object as CollisionObject3D
+	if road_collider.get_collision_layer_value(ROAD_COLLISION_LAYER):
+		_current_surface_type = SurfaceType.ROAD
+		return

8c801635bdd39ddf9881de83c6b56c29d5003ad2
mpdacey <80190692+mpdacey@users.noreply.github.com>
2025-12-03 21:09:33 +1100

    Added animations to fuel canister
    The fuel canister now rotates and pops up when moving.

--- scripts/fuel_placement_controller.gd
+++ scripts/fuel_placement_controller.gd
@@ -3,12 +3,14 @@ class_name FuelPlacementController
 
 @export var lane_follower : PathFollow3D
 @export var placement_follower : PathFollow3D
-@export var canister_distance : float = 120
+@export var canister_distance : float = 100
 @export var warp_shift_amount : float = 400
+@export var fuel_canister : Node3D
 
 func place_next_canister() -> void:
 	lane_follower.progress += canister_distance
 	placement_follower.progress_ratio = randf()
+	fuel_canister.get_node(fuel_canister.get_meta("animator")).play(&"grow")
 
 func warp_back() -> void:
 	lane_follower.progress -= warp_shift_amount

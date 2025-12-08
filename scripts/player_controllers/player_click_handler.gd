6ad5b931b97a51c033c67ead327393dbe5b6b014
mpdacey <80190692+mpdacey@users.noreply.github.com>
2025-12-08 19:13:16 +1100

    Clicking on the car while in game over resets the game

---
+++ scripts/player_controllers/player_click_handler.gd
@@ -0,0 +1,12 @@
+extends Node
+class_name PlayerClickHandler
+
+signal click_detected
+
+func _on_player_clickbox_area_input_event(_camera: Node, event : InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
+	if event is not InputEventMouseButton:
+		return
+	
+	event = event as InputEventMouseButton
+	if event.button_index == MOUSE_BUTTON_LEFT:
+		click_detected.emit()

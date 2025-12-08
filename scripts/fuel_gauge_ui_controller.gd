5eb39872365e8570cbb4e313a7857d3d1469b4a0
mpdacey <80190692+mpdacey@users.noreply.github.com>
2025-12-08 14:55:17 +1100

    Added warning symbols for fuel status

--- scripts/fuel_gauge_ui_controller.gd
+++ scripts/fuel_gauge_ui_controller.gd
@@ -1,6 +1,14 @@
 extends CanvasLayer
 
 @export var fuel_gauge: Slider
+@export var overflow_warning: TextureRect
+@export var underflow_warning: TextureRect
+@export_group("Warning Thresholds")
+@export_range(0, 1, 0.01) var full_fuel_threshold := 0.8
+@export_range(0, 1, 0.01) var low_fuel_threshold := 0.2
 
 func _on_fuel_amount_updated(remaining_fuel: float) -> void:
 	fuel_gauge.value = remaining_fuel
+	
+	overflow_warning.visible = remaining_fuel >= full_fuel_threshold
+	underflow_warning.visible = remaining_fuel <= low_fuel_threshold

1e793d078baafb1faa4850d3cd52c132674896ab
mpdacey <80190692+mpdacey@users.noreply.github.com>
2025-12-17 21:45:57 +1100

    Created title screen

--- scripts/scene_changer.gd
+++ scripts/scene_changer.gd
@@ -1,6 +1,8 @@
 extends Node
-
-const MAIN_SCENE = preload("res://scenes/main_scene.tscn")
+class_name SceneChanger
 
 func swap_to_main_scene() -> void:
-	get_tree().change_scene_to_packed(MAIN_SCENE)
+	get_tree().change_scene_to_packed(load("uid://kaxdymo06lyx"))
+
+func swap_to_title_scene() -> void:
+	get_tree().change_scene_to_packed(load("uid://3k54q4ik0b0u"))

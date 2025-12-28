1399f7326bdd19799bf2ec8392d825ffa094864a
mpdacey <80190692+mpdacey@users.noreply.github.com>
2025-12-28 17:21:48 +1100

    Added smoke emitter for low fuel.

--- scripts/exhaust/exhaust_controller.gd
+++ scripts/exhaust/exhaust_controller.gd
@@ -3,10 +3,15 @@ class_name ExhaustController
 
 @export_group("Left Exhaust References")
 @export var left_flame: ExhaustFlameController
+@export var left_smoke: CPUParticles3D
 @export_group("Right Exhaust References")
 @export var right_flame: ExhaustFlameController
+@export var right_smoke: CPUParticles3D
 
 func _on_engine_state_changed(state: FuelController.EngineState) -> void:
+	left_smoke.emitting = state == FuelController.EngineState.DRAINED
+	right_smoke.emitting = state == FuelController.EngineState.DRAINED
+	
 	match (state):
 		FuelController.EngineState.BOOST:
 			if not left_flame.is_ignited:

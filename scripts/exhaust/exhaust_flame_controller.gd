1fabf813697e80b0de3440822e8ae2ba99ab1871
mpdacey <80190692+mpdacey@users.noreply.github.com>
2025-12-28 16:44:16 +1100

    Added logic to exhaust flames.
    The flames now only appear when boosting.

--- scripts/exhaust/exhaust_flame_controller.gd
+++ scripts/exhaust/exhaust_flame_controller.gd
@@ -5,9 +5,14 @@ const IGNITE_KEY = &"flame-ignite"
 const FIZZLE_KEY = &"flame-fizzle"
 
 @export var transition_animator: AnimationPlayer
+var is_ignited : bool = false :
+	get():
+		return is_ignited
 
 func ignite() -> void:
 	transition_animator.play(IGNITE_KEY)
+	is_ignited = true
 
 func fizzle() -> void:
 	transition_animator.play(FIZZLE_KEY)
+	is_ignited = false

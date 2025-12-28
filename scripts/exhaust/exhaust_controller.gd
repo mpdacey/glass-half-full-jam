7dc7c06005b0047eae1421a65862356e1c4d78e9
mpdacey <80190692+mpdacey@users.noreply.github.com>
2025-12-28 16:44:16 +1100

    Added logic to exhaust flames.
    The flames now only appear when boosting.

---
+++ scripts/exhaust/exhaust_controller.gd
@@ -0,0 +1,18 @@
+extends Node
+class_name ExhaustController
+
+@export_group("Left Exhaust References")
+@export var left_flame: ExhaustFlameController
+@export_group("Right Exhaust References")
+@export var right_flame: ExhaustFlameController
+
+func _on_engine_state_changed(state: FuelController.EngineState) -> void:
+	match (state):
+		FuelController.EngineState.BOOST:
+			if not left_flame.is_ignited:
+				left_flame.ignite()
+				right_flame.ignite()
+		FuelController.EngineState.CRUISE:
+			if left_flame.is_ignited:
+				left_flame.fizzle()
+				right_flame.fizzle()

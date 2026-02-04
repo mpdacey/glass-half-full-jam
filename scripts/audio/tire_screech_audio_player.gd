60a4c28541d958b1d5390c99e1b478176f78c412
mpdacey <80190692+mpdacey@users.noreply.github.com>
2026-02-04 20:30:27 +1100

    Added off road effects
    Added dust clouds, car vibration and dirt driving sounds when off road.

--- scripts/audio/tire_screech_audio_player.gd
+++ scripts/audio/tire_screech_audio_player.gd
@@ -7,13 +7,3 @@ const CLIP_NAME_DIRT = "Dirt"
 func _on_surface_type_changed(state: SurfaceController.SurfaceType) -> void:
 	var interactive_stream : AudioStreamInteractive = stream
 	interactive_stream.initial_clip = state
-	return
-	var playback: AudioStreamPlaybackInteractive = get_stream_playback()
-	if not playback:
-		return
-	
-	match(state):
-		SurfaceController.SurfaceType.ROAD:
-			playback.switch_to_clip_by_name(CLIP_NAME_ROAD)
-		SurfaceController.SurfaceType.DIRT:
-			playback.switch_to_clip_by_name(CLIP_NAME_DIRT)

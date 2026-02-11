c1a9025ba4a570b31dbf8d6ce693222203244dcf
mpdacey <80190692+mpdacey@users.noreply.github.com>
2026-02-11 19:58:32 +1100

    Added progressive traffic intensity
    Rather than traffic being hectic from the start, it will slowly build up as time progresses.

--- scripts/road_segment_controller.gd
+++ scripts/road_segment_controller.gd
@@ -2,17 +2,19 @@ extends Node3D
 class_name RoadSegmentController
 
 const ROAD_SEGMENT_LENGTH = 200
+const MAX_TRAFFIC = 7
 
 @export var traffic_scenes: Array[PackedScene]
 @export_group("References")
 @export var traffic_paths: Array[Path3D]
 var minimum_distance_between_traffic := 25.0
+var traffic_count : int = 2
 
 func dress_road_segment() -> void:
 	_clean_road_segment()
 	
 	for lane_index in traffic_paths.size():
-		var num_of_vehicles := randi_range(7,11)
+		var num_of_vehicles := randi_range(traffic_count, roundi(traffic_count * 1.4))
 		
 		var potential_placements : Array[float] = []
 		for i in num_of_vehicles:
@@ -29,6 +31,7 @@ func dress_road_segment() -> void:
 			var vehicle : TrafficCoreController = traffic_scenes.pick_random().instantiate()
 			traffic_paths[lane_index].add_child(vehicle)
 			vehicle.set_traffic_properties(placement * divisor, lane_index == 0)
+	traffic_count = mini(traffic_count + 1, MAX_TRAFFIC)
 
 func halt_traffic() -> void:
 	for path in traffic_paths:

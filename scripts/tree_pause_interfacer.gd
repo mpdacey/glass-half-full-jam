28197e6f8909ac123881766ddec4e3e4ed0e1337
mpdacey <80190692+mpdacey@users.noreply.github.com>
2026-02-21 22:25:49 +1100

    Added resume time effect when finished pausing.

--- scripts/tree_pause_interfacer.gd
+++ scripts/tree_pause_interfacer.gd
@@ -1,8 +1,19 @@
 extends Node
 class_name EnginePauseInterfacer
 
+@export var fade_resume_duration : float = 0.5
+
 func pause_engine() -> void:
 	get_tree().paused = true
 
-func resume_engine() -> void:
+func resume_engine(fade_resume: bool = false) -> void:
 	get_tree().paused = false
+	
+	if not fade_resume:
+		return
+	
+	Engine.time_scale = 0
+	var tween := get_tree().create_tween()
+	tween.tween_property(Engine, "time_scale", 1.0, fade_resume_duration)
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.set_trans(Tween.TRANS_SINE)

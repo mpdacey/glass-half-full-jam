5efbc163ba1c097a5f07eeb7eb4358ecd0ae56e7
mpdacey <80190692+mpdacey@users.noreply.github.com>
2026-03-03 22:08:42 +1100

    Added scrolling animation for whole number values.
    One's through ten thousands digits now have a tween that plays when they increment.

--- scripts/odometer_gauge_ui_controller.gd
+++ scripts/odometer_gauge_ui_controller.gd
@@ -2,6 +2,7 @@ extends Node
 
 const DISPLAY_NUMBER_KEY = &"display_number"
 const MAX_SCORE = 99999.9
+const NUMBER_CHANGE_TIME = 0.15
 @export var odometer_values : Array[Sprite2D]
 
 func update_odometer(kilometres_travelled: float) -> void:
@@ -9,8 +10,15 @@ func update_odometer(kilometres_travelled: float) -> void:
 	
 	var decimal_value := score * 10
 	odometer_values[0].set_instance_shader_parameter(DISPLAY_NUMBER_KEY, decimal_value)
-	#print(odometer_values[0].get_instance_shader_parameter(DISPLAY_NUMBER_KEY))
 	
 	for i in range(1, odometer_values.size()):
-		var value : float = fmod(floor(score * pow(0.1, (i - 1))), 10)
-		odometer_values[i].set_instance_shader_parameter(DISPLAY_NUMBER_KEY, value)
+		var new_value : float = floor(score * pow(0.1, (i - 1)))
+		var current_value : float = odometer_values[i].get_instance_shader_parameter(DISPLAY_NUMBER_KEY)
+		
+		if new_value == current_value:
+			continue
+		
+		var tween := create_tween()
+		var set_shader_value_callable := func(value: float) -> void:
+			odometer_values[i].set_instance_shader_parameter(DISPLAY_NUMBER_KEY, value)
+		tween.tween_method(set_shader_value_callable, current_value, new_value, NUMBER_CHANGE_TIME)

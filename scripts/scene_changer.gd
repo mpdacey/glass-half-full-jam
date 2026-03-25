aa988785f7c0a052fab5f355d9155aee154cad71
mpdacey <80190692+mpdacey@users.noreply.github.com>
2026-03-25 17:28:36 +1100

    Swapped preload for load
    This will remove the title and play scenes from cached memory when they shouldn't be needed.

--- scripts/scene_changer.gd
+++ scripts/scene_changer.gd
@@ -4,19 +4,21 @@ class_name SceneChanger
 signal play_scene_loaded
 signal title_scene_loaded
 
-const PLAY_SCENE = preload("uid://dpfq7m4viuno")
-const TITLE_SCENE = preload("uid://3k54q4ik0b0u")
+const PLAY_SCENE_UID = "uid://dpfq7m4viuno"
+const TITLE_SCENE_UID = "uid://3k54q4ik0b0u"
 
 @export var scene_container : Node
 
-
 func swap_to_play_scene() -> void:
-	_switch_dynamic_scene(PLAY_SCENE)
+	_switch_dynamic_scene(_load_scene(PLAY_SCENE_UID))
 	play_scene_loaded.emit()
 
 func swap_to_title_scene() -> void:
-	_switch_dynamic_scene(TITLE_SCENE)
+	_switch_dynamic_scene(_load_scene(TITLE_SCENE_UID))
 	title_scene_loaded.emit()
+
+func _load_scene(uid: String) -> PackedScene:
+	return load(uid)
 
 func _switch_dynamic_scene(scene: PackedScene) -> void:
 	scene_container.get_child(0).queue_free()

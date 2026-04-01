69ff40889bc4c564ba650ff1986e23ee06786def
mpdacey <80190692+mpdacey@users.noreply.github.com>
2026-04-01 21:49:35 +1100

    Created highscore manager
    The highscore manager updates the highscore if a new highscore has been reached.
    I don't like how its got a direct link to the drive controller, but my fatigued brain can't think of a way to link them together for a one shot connection after a game over has occured (to prevent quiting creating a new highscore)

---
+++ scripts/highscore_manager.gd
@@ -0,0 +1,22 @@
+extends Node
+class_name HighscoreManager
+
+signal new_highscore_set
+
+@export var drive_controller: DriveController
+
+func _on_gameover() -> void:
+	drive_controller.distance_traveled.connect(_update_highscore, CONNECT_ONE_SHOT)
+	drive_controller.emit_distance_travelled()
+
+func _update_highscore(metres_travelled: float) -> void:
+	metres_travelled = floor(metres_travelled * 10) * 0.1
+	
+	var current_highscore : float = UserConfigManager.get_config_value(UserConfigManager.HIGHSCORE_KEY)
+	if current_highscore >= metres_travelled:
+		return
+	
+	UserConfigManager.set_config_value(UserConfigManager.HIGHSCORE_KEY, metres_travelled)
+	UserConfigManager.save_config_file()
+	
+	new_highscore_set.emit()

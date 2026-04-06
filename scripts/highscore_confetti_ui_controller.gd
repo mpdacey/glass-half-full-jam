aab45fe249185fb0799fce91d4e05ac595759292
mpdacey <80190692+mpdacey@users.noreply.github.com>
2026-04-06 11:42:57 +1000

    Added new sound effects for confetti cannon

--- scripts/highscore_confetti_ui_controller.gd
+++ scripts/highscore_confetti_ui_controller.gd
@@ -5,6 +5,9 @@ signal confetti_popped
 
 func pop_confetti() -> void:
 	for child in get_children():
+		if child.get_child_count() <= 0 or child.get_child(0) is not CPUParticles2D:
+			continue
+		
 		var particles: CPUParticles2D = child.get_child(0)
 		particles.emitting = true
 	

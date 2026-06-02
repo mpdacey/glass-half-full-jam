a90c5aa233030e1d3f03262f876afd6fde3ebf57
mpdacey <80190692+mpdacey@users.noreply.github.com>
2026-06-02 15:05:20 +1000

    Reverted checking connection on start.
    This was theoretically a good idea but man it was horrible for game feel. Having to wait half a second (and possibly more) after pressing start ruined the whole flow. If the game is going to check connection, it should do so at points where the player doesn't have control.

--- scripts/global/global_constants.gd
+++ scripts/global/global_constants.gd
@@ -7,7 +7,6 @@ const LEADERBOARD_ID = "CgkInsTW6vgPEAIQAA"
 enum SpendLifeResponses
 {
 	OK,
-	NO_LIVES,
 	NO_CONNECTION,
 	NO_RESPONSE
 }
--- scripts/global/global_signal_bus.gd
+++ scripts/global/global_signal_bus.gd
@@ -18,5 +18,5 @@ signal trigger_score_request
 #region Lives System
 signal spend_life_requested
 signal spend_life_granted
-signal spend_life_denied(responce: GlobalConstants.SpendLifeResponses)
+signal spend_life_denied
 #endregion

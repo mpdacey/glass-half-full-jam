0fe5492376bdf9740666d6d5b9833f8055a61d2d
mpdacey <80190692+mpdacey@users.noreply.github.com>
2026-06-05 18:17:34 +1000

    Internet dependant animations play correctly when connected.
    Before, these internet dependant animations would play as soon as an internet connection was established within the scene. This posed multiple problems. First of which was that if the internet connection was established before the scene loaded, they would not be called at all. Secondly, if the internet connection was established after the scene has loaded, it would play the animation regardless of when it should appropriately play during any other accompanying animations, ruining the aesthetic of the scene. Now the animation only play if the internet has been established AND when it is appropriate to play (called upon to play).

--- scripts/global/internet_connection_checker.gd
+++ scripts/global/internet_connection_checker.gd
@@ -1,17 +1,32 @@
 extends Node
 
+signal connection_established
+
 @export var connecting_canvas: CanvasLayer
 @export var timeout_timer : Timer 
+var _current_status : GlobalConstants.TimeConnectionResponses = GlobalConstants.TimeConnectionResponses.NO_CONNECTION
 
 func check_connection() -> void:
+	_current_status = GlobalConstants.TimeConnectionResponses.NO_CONNECTION
 	timeout_timer.start()
 	connecting_canvas.show()
 	GlobalTimeInterfacer.status_found.connect(_on_status_found, CONNECT_ONE_SHOT)
 	GlobalTimeInterfacer.ping_global_time()
 
+func emit_when_established() -> void:
+	if _current_status == GlobalConstants.TimeConnectionResponses.OK:
+		connection_established.emit()
+		return
+	
+	var established_connection_signal: Signal = GlobalSignalBus.time_established_connection
+	var is_already_connected := established_connection_signal.is_connected(connection_established.emit)
+	if not is_already_connected:
+		established_connection_signal.connect(connection_established.emit, CONNECT_ONE_SHOT)
+
 func _on_status_found(status: GlobalConstants.TimeConnectionResponses) -> void:
 	timeout_timer.stop()
 	connecting_canvas.hide()
+	_current_status = status
 	
 	match status:
 		GlobalConstants.TimeConnectionResponses.OK:

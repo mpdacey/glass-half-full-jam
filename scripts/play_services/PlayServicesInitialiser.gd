e62b3b98f111a42f4feec70f2d82c9f7ae5d1fed
mpdacey <80190692+mpdacey@users.noreply.github.com>
2026-06-08 15:08:55 +1000

    Added frictionless 0 click sign in

--- scripts/play_services/PlayServicesInitialiser.gd
+++ scripts/play_services/PlayServicesInitialiser.gd
@@ -1,4 +1,17 @@
 extends Node
+class_name PlayServiceInitialiser
+
+@export var play_games_sign_in_client : PlayGamesSignInClient
 
 func _enter_tree() -> void:
 	GodotPlayGameServices.initialize()
+
+func _ready() -> void:
+	android_authentication()
+
+func android_authentication() -> void:
+	if not GodotPlayGameServices.android_plugin:
+		printerr("GodotPlayGameServices plugin not found.")
+		return
+	
+	play_games_sign_in_client.is_authenticated()

898ef1523121777248df4e70e2f685431c2ea4c5
mpdacey <80190692+mpdacey@users.noreply.github.com>
2026-06-29 17:25:33 +1000

    Fixed button group being broken

--- scripts/snackbar_button_controller.gd
+++ scripts/snackbar_button_controller.gd
@@ -6,8 +6,12 @@ signal snackbar_button_pressed
 @export var button_icon: Texture2D
 @export_group("References")
 @export var button_icon_node: TextureRect
+@export var button_reference: Button
+var _button_group : ButtonGroup = preload("uid://ckjn10ul5kvve")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if button_icon != null:
 		button_icon_node.texture = button_icon
+	
+	button_reference.button_group = _button_group

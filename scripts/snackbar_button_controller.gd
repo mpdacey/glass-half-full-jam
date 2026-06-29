f4e70010f9ed87de28d21ae3b9751d41a1d338c5
mpdacey <80190692+mpdacey@users.noreply.github.com>
2026-06-29 17:47:37 +1000

    Added released signal

--- scripts/snackbar_button_controller.gd
+++ scripts/snackbar_button_controller.gd
@@ -2,6 +2,7 @@ extends Node
 class_name SnackbarButtonController
 
 signal snackbar_button_pressed
+signal snackbar_button_released
 
 @export var button_icon: Texture2D
 @export_group("References")
@@ -9,9 +10,14 @@ signal snackbar_button_pressed
 @export var button_reference: Button
 var _button_group : ButtonGroup = preload("uid://ckjn10ul5kvve")
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if button_icon != null:
 		button_icon_node.texture = button_icon
 	
 	button_reference.button_group = _button_group
+
+func _on_button_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		snackbar_button_pressed.emit()
+	else:
+		snackbar_button_released.emit()

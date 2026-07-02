6645d2223dae2dcdf1e4aec5a725aa71fc8fa1b2
mpdacey <80190692+mpdacey@users.noreply.github.com>
2026-07-02 18:22:26 +1000

    Added underline light to buttons.
    This underline light better illustrates which button is selected, and also emulates what console car buttons look like.

--- scripts/snackbar_button_controller.gd
+++ scripts/snackbar_button_controller.gd
@@ -8,7 +8,10 @@ signal snackbar_button_released
 @export_group("References")
 @export var button_icon_node: TextureRect
 @export var button_reference: Button
+@export var button_underline: Panel
 var _button_group : ButtonGroup = preload("uid://ckjn10ul5kvve")
+var _lit_underline_stylebox: StyleBoxFlat = preload("uid://bfjnouwh84kb")
+var _unlit_underline_stylebox: StyleBoxFlat = preload("uid://bvnq8hjtkstin")
 
 func _ready() -> void:
 	if button_icon != null:
@@ -17,7 +20,15 @@ func _ready() -> void:
 	button_reference.button_group = _button_group
 
 func _on_button_toggled(toggled_on: bool) -> void:
+	_set_underline(toggled_on)
+	
 	if toggled_on:
 		snackbar_button_pressed.emit()
 	else:
 		snackbar_button_released.emit()
+
+func _set_underline(toggled_on: bool) -> void:
+	if toggled_on:
+		button_underline.add_theme_stylebox_override(&"panel", _lit_underline_stylebox)
+	else:
+		button_underline.add_theme_stylebox_override(&"panel", _unlit_underline_stylebox)

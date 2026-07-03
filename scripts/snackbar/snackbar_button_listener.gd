b748d8b7b00f7a0f289ff4ac2336e1b203abac72
mpdacey <80190692+mpdacey@users.noreply.github.com>
2026-07-03 12:58:16 +1000

    Created snackbar button listener for snackbar updates.

---
+++ scripts/snackbar/snackbar_button_listener.gd
@@ -0,0 +1,23 @@
+extends Node
+class_name SnackbarButtonListener
+
+signal target_toggled_on
+signal target_toggled_off
+
+const SNACKBAR_CHANGED_SIGNAL_NAME = &"snackbar_button_pressed"
+
+@export var target_button_type: SnackbarUIController.SnackbarButtonType
+var _is_toggled : bool = false
+
+func _enter_tree() -> void:
+	var global_signal_listener := GlobalSignalInterfacer.new(SNACKBAR_CHANGED_SIGNAL_NAME)
+	global_signal_listener.global_signal_emitted_with_value.connect(_on_button_type_changed)
+	add_child(global_signal_listener)
+
+func _on_button_type_changed(new_type: SnackbarUIController.SnackbarButtonType) -> void:
+	if new_type == target_button_type and not _is_toggled:
+		_is_toggled = true
+		target_toggled_on.emit()
+	elif new_type != target_button_type and _is_toggled:
+		_is_toggled = false
+		target_toggled_off.emit()
--- scripts/snackbar/snackbar_ui_controller.gd
+++ scripts/snackbar/snackbar_ui_controller.gd
@@ -5,9 +5,27 @@ const DISPLAY_KEY = &"show"
 const DISMISS_KEY = &"hide"
 
 @export var animator: AnimationPlayer
+@export var global_signal_emitter: GlobalSignalInterfacer
+
+enum SnackbarButtonType {
+	DRIVE = 0,
+	LEADERBOARD = 1,
+	SETTINGS = 2,
+	SHOP = 3,
+	GARAGE = 4
+}
+
+var _current_pressed : SnackbarButtonType = SnackbarButtonType.DRIVE
 
 func display() -> void:
 	animator.play(DISPLAY_KEY)
 
 func dismiss() -> void:
 	animator.play(DISMISS_KEY)
+
+func _on_snackbar_button_pressed(button_type: SnackbarButtonType) -> void:
+	if button_type == _current_pressed:
+		return
+	
+	_current_pressed = button_type
+	global_signal_emitter.emit_global_signal_with_value(_current_pressed)

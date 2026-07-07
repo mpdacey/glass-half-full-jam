dc6d08c57d77490893ab75068449749cf527d80d
mpdacey <80190692+mpdacey@users.noreply.github.com>
2026-07-07 13:25:23 +1000

    Created the purchase controller.
    This class interfaces with the billing client and listens for responses.

---
+++ scripts/billing/purchase_controller.gd
@@ -0,0 +1,78 @@
+extends Node
+class_name PurchaseController
+
+const PREMIUM_ITEM_ID = "TODO: Replace this string"
+
+var billing_client: BillingClient
+func _ready() -> void:
+	billing_client = BillingClient.new()
+	billing_client.connected.connect(_on_connected) # No params
+	billing_client.disconnected.connect(_on_disconnected) # No params
+	billing_client.connect_error.connect(_on_connect_error) # response_code: int, debug_message: String
+	billing_client.query_product_details_response.connect(_on_query_product_details_response) # response: Dictionary
+	billing_client.query_purchases_response.connect(_on_query_purchases_response) # response: Dictionary
+	billing_client.on_purchase_updated.connect(_on_purchase_updated) # response: Dictionary
+	billing_client.consume_purchase_response.connect(_on_consume_purchase_response) # response: Dictionary
+	billing_client.acknowledge_purchase_response.connect(_on_acknowledge_purchase_response) # response: Dictionary
+
+	billing_client.start_connection()
+
+func _on_connected() -> void:
+	billing_client.query_product_details([PREMIUM_ITEM_ID], BillingClient.ProductType.INAPP)
+
+func _on_disconnected() -> void:
+	pass
+
+func _on_connect_error(response_code: int, debug_message: String) -> void:
+	_print_error("Failed to connect to billing client", response_code, debug_message)
+
+func _on_query_product_details_response(response: Dictionary) -> void:
+	if response.response_code == BillingClient.BillingResponseCode.OK:
+		print("Available products")
+		for available_product : Variant in response.product_details:
+			print(available_product)
+	else:
+		_print_error("Product details query failed", response.response_code, response.debug_message)
+
+func _query_purchases() -> void:
+	billing_client.query_purchases(BillingClient.ProductType.INAPP)
+
+func _on_query_purchases_response(response: Dictionary) -> void:
+	if response.response_code == BillingClient.BillingResponseCode.OK:
+		print("Purchase query successful")
+		for purchase: Dictionary in response.purchases:
+			_process_purchase(purchase)
+	else:
+		_print_error("Purchase update error", response.response_code, response.debug_message)
+
+func _process_purchase(purchase: Dictionary) -> void:
+	if (
+		PREMIUM_ITEM_ID in purchase.product_ids 
+		and purchase.purchase_state == BillingClient.PurchaseState.PURCHASED
+		and not purchase.is_acknowledged
+	):
+		billing_client.acknowledge_purchase(purchase.purchase_token)
+
+func _on_purchase_updated(response: Dictionary) -> void:
+	pass
+
+func _on_consume_purchase_response(response: Dictionary) -> void:
+	pass
+
+func _on_acknowledge_purchase_response(response: Dictionary) -> void:
+	if response.response_code == BillingClient.BillingResponseCode.OK:
+		print("Acknowledge purchase success")
+		_handle_purchase_token(response.token, true)
+	else:
+		_print_error("Acknowledge purchase failed", response.response_code, response.debug_message)
+
+func _handle_purchase_token(purchase_token: String, purchase_successful: bool) -> void:
+	pass
+
+func _on_purchase_button_pressed() -> void:
+	pass
+
+func _print_error(custom_message: String, response_code: int, debug_message: String) -> void:
+		print(custom_message)
+		print("response code: ", response_code)
+		print("debug_message: ", debug_message)

a460eab859f53aac6651396833f1c5a5d777847b
mpdacey <80190692+mpdacey@users.noreply.github.com>
2026-07-15 16:01:50 +1000

    Added auto press function to title snackbar.
    When the title scene is loaded, the scene changer adds meta data to it so that it can automatically open a snackbar view without user input. This will be used to open the shop from the play scene when the player runs out of lives.

--- scripts/scene_changer.gd
+++ scripts/scene_changer.gd
@@ -13,13 +13,18 @@ func swap_to_play_scene() -> void:
 	_switch_dynamic_scene(_load_scene(PLAY_SCENE_UID))
 	play_scene_loaded.emit()
 
-func swap_to_title_scene() -> void:
-	_switch_dynamic_scene(_load_scene(TITLE_SCENE_UID))
+func swap_to_title_scene(snackbar_option: SnackbarUIController.SnackbarButtonType = SnackbarUIController.SnackbarButtonType.DRIVE) -> void:
+	var title_node := _switch_dynamic_scene(_load_scene(TITLE_SCENE_UID))
+	title_node.set_meta(GlobalConstants.SNACKBAR_BUTTON_META_KEY, snackbar_option)
 	title_scene_loaded.emit()
 
 func _load_scene(uid: String) -> PackedScene:
 	return load(uid)
 
-func _switch_dynamic_scene(scene: PackedScene) -> void:
+func _switch_dynamic_scene(scene: PackedScene) -> Node:
 	scene_container.get_child(0).queue_free()
-	scene_container.call_deferred("add_child", scene.instantiate())
+	
+	var new_scene : Node = scene.instantiate()
+	scene_container.call_deferred("add_child", new_scene)
+	
+	return new_scene

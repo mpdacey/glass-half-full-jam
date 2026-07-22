2d6be936018f07d1651409c13bf819b0ab95dd10
mpdacey <80190692+mpdacey@users.noreply.github.com>
2026-07-22 14:03:23 +1000

    Fixed shop icon being available while having premium

--- scripts/global/premium_signal_listener.gd
+++ scripts/global/premium_signal_listener.gd
@@ -9,3 +9,4 @@ func _enter_tree() -> void:
 	PurchaseController.premium_purchase_successful.connect(premium_purchase_successful.emit)
 	PurchaseController.premium_purchase_failed.connect(premium_purchase_failed.emit)
 	PurchaseController.premium_status_found.connect(premium_status_found.emit)
+	premium_status_found.emit(PurchaseController.has_premium)

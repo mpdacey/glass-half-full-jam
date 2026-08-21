6364c675ccf76dd257b96056aa1280229218a223
mpdacey <80190692+mpdacey@users.noreply.github.com>
2026-08-21 15:57:47 +1000

    Added ability to display and dismiss credits in options.

--- scripts/credits_manual_ui_controller.gd
+++ scripts/credits_manual_ui_controller.gd
@@ -9,4 +9,9 @@ func open_book() -> void:
 	book_animator.play(OPEN_ANIMATION_KEY)
 
 func close_book() -> void:
-	book_animator.play(CLOSE_ANIMATION_KEY)
+	if (
+		book_animator.current_animation != CLOSE_ANIMATION_KEY
+		and not book_animator.is_playing()
+		and visible
+	):
+		book_animator.play(CLOSE_ANIMATION_KEY)

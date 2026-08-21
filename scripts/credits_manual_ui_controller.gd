f46d6fb66c690d81b8be14e57b1337f7d397f150
mpdacey <80190692+mpdacey@users.noreply.github.com>
2026-08-21 15:24:36 +1000

    Updated credits ui to Godot 4.7

---
+++ scripts/credits_manual_ui_controller.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+const OPEN_ANIMATION_KEY = &"open"
+const CLOSE_ANIMATION_KEY = &"close"
+
+@export var book_animator: AnimationPlayer
+
+func open_book() -> void:
+	book_animator.play(OPEN_ANIMATION_KEY)
+
+func close_book() -> void:
+	book_animator.play(CLOSE_ANIMATION_KEY)

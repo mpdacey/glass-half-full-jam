efaedcbbb63d303bdeb86f03406c106cfced3e94
mpdacey <80190692+mpdacey@users.noreply.github.com>
2026-09-18 17:18:10 +1000

    Added confetti cannons for each type of highscore.

--- scripts/highscore_confetti_ui_controller.gd
+++ scripts/highscore_confetti_ui_controller.gd
@@ -3,8 +3,11 @@ class_name HighscoreConfettiUI
 
 signal confetti_popped
 
-func pop_confetti() -> void:
-	for child in get_children():
+@export var confetti_cannon_containers: Dictionary[PlayGamesLeaderboardVariant.TimeSpan, Control]
+
+func pop_confetti(highscore_type: PlayGamesLeaderboardVariant.TimeSpan = PlayGamesLeaderboardVariant.TimeSpan.TIME_SPAN_ALL_TIME) -> void:
+	var cannon_container := confetti_cannon_containers[highscore_type]
+	for child in cannon_container.get_children():
 		if child.get_child_count() <= 0 or child.get_child(0) is not CPUParticles2D:
 			continue
 		
